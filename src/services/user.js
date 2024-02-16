0faa9b53b32adcff783a17c45581df5d7858e560
JoaoVitor <joaovitor2201@hotmail.com>
2024-02-16 15:04:05 -0300

    Teste para nao inserir usuario sem email

--- src/services/user.js
+++ src/services/user.js
@@ -1,14 +1,17 @@
 module.exports = (app) => {
-    const findAll = () => {
-        return app.db('users').select();
-    }
+	const findAll = () => {
+		return app.db('users').select();
+	}
 
-    const save = async (user) => {
-        if (!user.name) {
-            return { error: 'Nome é um atributo obrigatorio' }
-        }
-        return app.db('users').insert(user, '*')
-    };
+	const save = async (user) => {
+		if (!user.name) {
+			return { error: 'Nome é um atributo obrigatorio' }
+		}
+		if (!user.mail) {
+			return { error: 'Email é um atributo obrigatorio' }
+		}
+		return app.db('users').insert(user, '*')
+	};
 
-    return { save, findAll }
+	return { save, findAll }
 }
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -3,29 +3,37 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 test("Deve listar todos os usuarios", () => {
-    return request(app).get('/users')
-        .then((res) => {
-            expect(res.status).toBe(200);
-            expect(res.body.length).toBeGreaterThan(0);
-        });
+	return request(app).get('/users')
+		.then((res) => {
+			expect(res.status).toBe(200);
+			expect(res.body.length).toBeGreaterThan(0);
+		});
 })
 
 test('deve inserir usuario com sucesso', () => {
-    const mail = `${Date.now()}@mail.com`
-    return request(app).post('/users')
-        .send({ name: 'Walter Mitty', mail, password: '123123' })
-        .then((res) => {
-            expect(res.status).toBe(201);
-            expect(res.body.name).toBe('Walter Mitty')
-        })
+	const mail = `${Date.now()}@mail.com`
+	return request(app).post('/users')
+		.send({ name: 'Walter Mitty', mail, password: '123123' })
+		.then((res) => {
+			expect(res.status).toBe(201);
+			expect(res.body.name).toBe('Walter Mitty')
+		})
 })
 
 test('Nao deve inserir usuario sem nome', () => {
-    const mail = `${Date.now()}@mail.com`
-    return request(app).post('/users')
-        .send({ mail, password: '123123' })
-        .then((res) => {
-            expect(res.status).toBe(400);
-            expect(res.body.error).toBe('Nome é um atributo obrigatorio')
-        })
+	const mail = `${Date.now()}@mail.com`
+	return request(app).post('/users')
+		.send({ mail, password: '123123' })
+		.then((res) => {
+			expect(res.status).toBe(400);
+			expect(res.body.error).toBe('Nome é um atributo obrigatorio')
+		})
+})
+
+test('Nao deve inserir usuario sem email', async () => {
+	const result = await request(app).post('/users')
+		.send({ name: 'Walter Mitty', password: '123123' })
+
+	expect(result.status).toBe(400);
+	expect(result.body.error).toBe('Email é um atributo obrigatorio')
 })

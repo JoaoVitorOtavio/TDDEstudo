9bf9cbad3a302f5387be4b5d8ec5db62c0be9dd8
JoaoVitor <joaovitor2201@hotmail.com>
2024-02-27 20:58:13 -0300

    Criando teste para listar apenas as transacoes do usuario

---
+++ test/routes/transaction.test.js
@@ -0,0 +1,49 @@
+const request = require("supertest");
+const jwt = require("jwt-simple")
+const app = require('../../src/app');
+
+const MAIN_ROUTE = '/v1/transactions';
+
+let user;
+let accUser;
+
+let user2;
+let accUser2;
+
+
+beforeAll(async () => {
+  await app.db('transactions').del();
+  await app.db('accounts').del();
+  await app.db('users').del();
+
+  const users = await app.db('users').insert([
+    { name: 'User #1', mail: 'user@mail.com', password: '$2a$10$1GOjmbpxFMtPDMMKZmMPR.tiJtf43xhtVUi8Tey/bSwoheQcJV05S' },
+    { name: 'User #2', mail: 'user2@mail.com', password: '$2a$10$1GOjmbpxFMtPDMMKZmMPR.tiJtf43xhtVUi8Tey/bSwoheQcJV05S' }
+  ], '*');
+
+  [user, user2] = users;
+  delete user.password;
+
+  user.token = jwt.encode(user, 'Secret super secreto')
+
+  const accs = await app.db('accounts').insert([
+    { name: 'Acc #1', user_id: user.id },
+    { name: 'Acc #2', user_id: user2.id },
+  ], '*');
+
+  [accUser, accUser2] = accs;
+});
+
+test('Deve listar apenas as transacoes do usuario', () => {
+  return app.db('transactions').insert([
+    { description: 'T1', date: new Date(), ammount: 100, type: 'I', acc_id: accUser.id },
+    { description: 'T2', date: new Date(), ammount: 300, type: 'O', acc_id: accUser2.id },
+  ]).then(() => request(app).get(MAIN_ROUTE)
+    .set('authorization', `bearer ${user.token}`)
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveLength(1);
+      expect(res.body[0].description).toBe('T1')
+    })
+  )
+})
